51831532cfe19a496f26b4ab55c03acb49c3d267
Dmitry Fil <monkdaf@gmail.com>
2016-08-13 17:54:11 +0300

    Курс JS: Обработка ошибок. Заготовка для практики урока 1 Деревья

---
+++ js_errors/01_trees/Tree.js
@@ -0,0 +1,33 @@
+﻿class Tree {
+  constructor(key, meta, parent) {
+    this.parent = parent;
+    this.key = key;
+    this.meta = meta;
+    this.children = new Map();
+  }
+
+  getKey() {
+    return this.key;
+  }
+
+  getMeta() {
+    return this.meta;
+  }
+
+  addChild(key, meta) {
+    const child = new Tree(key, meta, this);
+    this.children.set(key, child);
+
+    return child;
+  }
+
+  getChild(key) {
+    return this.children.get(key);
+  }
+
+  // BEGIN (write your solution here)
+  
+  // END
+}
+
+export default Tree;
---
+++ js_errors/01_trees/test.js
@@ -0,0 +1,63 @@
+﻿import assert from 'assert';
+
+import Tree from './Tree';
+
+describe('Tree', () => {
+  let tree;
+
+  before(() => {
+    tree = new Tree('/');
+    tree.addChild('var')
+      .addChild('lib')
+      .addChild('run');
+    tree.addChild('etc');
+    tree.addChild('home');
+  });
+
+  it('#hasChildren', () => {
+    assert.ok(tree.hasChildren());
+  });
+
+  it('#hasChild', () => {
+    assert.ok(!tree.hasChild('/'));
+    assert.ok(tree.hasChild('etc'));
+  });
+
+  it('#getParent', () => {
+    const subtree = tree.getChild('var');
+    assert.equal(subtree && subtree.getParent(), tree);
+  });
+
+  it('#getChildren', () => {
+    const dirs = tree.getChildren().map(child => child.getKey());
+    assert.deepEqual(dirs, ['var', 'etc', 'home']);
+  });
+
+  it('#getChild', () => {
+    const subtree = tree.getChild('var');
+    assert.equal(subtree && subtree.getKey(), 'var');
+  });
+
+  it('#getChild undefined', () => {
+    const subtree = tree.getChild('undefined');
+    assert.equal(subtree, undefined);
+  });
+
+  it('#getDeepChild', () => {
+    const subtree = tree.getDeepChild(['var', 'lib']);
+    assert.equal(subtree && subtree.getKey(), 'lib');
+    const parent = subtree && subtree.getParent();
+    assert.equal(parent && parent.getKey(), 'var');
+  });
+
+  it('#getDeepChild undefined', () => {
+    const subtree = tree.getDeepChild(['var', 'lib', 'one', 'two']);
+    assert.equal(subtree, undefined);
+  });
+
+  it('#removeChild', () => {
+    const subtree = tree.getChild('var');
+    subtree && subtree.removeChild('lib');
+    assert.ok(subtree && !subtree.hasChildren());
+  });
+});
