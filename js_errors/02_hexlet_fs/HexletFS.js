f4b99c27d62ab880298a5c469f506c09a13040b4
Dmitry Fil <monkdaf@gmail.com>
2016-08-14 17:59:44 +0300

    Заготовка для практики урока 2

---
+++ js_errors/02_hexlet_fs/HexletFS.js
@@ -0,0 +1,15 @@
+﻿import Tree from 'hexlet-trees';
+
+// BEGIN (write your solution here)
+
+// END
+
+export default class {
+  constructor() {
+    this.tree = new Tree('/');
+  }
+
+  // BEGIN (write your solution here)
+  
+  // END
+}
---
+++ js_errors/02_hexlet_fs/test.js
@@ -0,0 +1,45 @@
+﻿import assert from 'assert';
+
+import HexletFs from './HexletFs';
+
+
+describe('FS', () => {
+  let files;
+
+  beforeEach(() => {
+    files = new HexletFs();
+  });
+
+  it('#mkdirSync', () => {
+    assert.ok(!files.isDirectory('/etc'));
+
+    files.mkdirSync('/etc');
+    assert.ok(files.isDirectory('/etc'));
+
+    files.mkdirSync('/etc/nginx');
+    assert.ok(files.isDirectory('/etc/nginx'));
+  });
+
+  it('#mkdirSync2', () => {
+    assert.ok(!files.isDirectory('/var//'));
+
+    files.mkdirSync('/var/');
+    assert.ok(files.isDirectory('/var////'));
+    assert.ok(files.isDirectory('/var'));
+
+    files.mkdirSync('/var//log//////');
+    assert.ok(files.isDirectory('/var/log'));
+    assert.ok(files.isDirectory('/var///log'));
+  });
+
+  it('#touchSync', () => {
+    assert.ok(!files.isFile('/file.txt'));
+
+    files.touchSync('/file.txt');
+    assert.ok(files.isFile('/file.txt'));
+
+    files.mkdirSync('/etc');
+    files.touchSync('/etc/bashrc');
+    assert.ok(files.isFile('/etc/bashrc'));
+  });
+});
