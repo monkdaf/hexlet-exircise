f11dcd99b06723e3bcbb091881adcecb10b0b18e
Dmitry Fil <monkdaf@gmail.com>
2016-08-17 21:57:12 +0300

    Урок 3 Create table

---
+++ postgresql-ddl/03_create_table/solution.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS "cars";
+
+-- BEGIN (write your solution here)
+CREATE TABLE cars(
+    id integer,
+    name character varying,
+    price numeric,
+    created_at timestamp);
+-- END
---
+++ postgresql-ddl/03_create_table/solution_teacher.sql
@@ -0,0 +1,8 @@
+﻿-- BEGIN
+CREATE TABLE cars (
+    id integer,
+    name character varying,
+    price numeric,
+    created_at timestamp
+);
+-- END
