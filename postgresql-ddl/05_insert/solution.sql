b1590c96232f144dbcd936b73945dacb6be1ba52
Dmitry Fil <monkdaf@gmail.com>
2016-08-17 22:37:17 +0300

    Урок 5 insert

---
+++ postgresql-ddl/05_insert/solution.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE IF EXISTS "cars";
+
+CREATE TABLE cars (
+    id integer PRIMARY KEY,
+    name character varying UNIQUE NOT NULL,
+    price numeric
+);
+
+-- BEGIN (write your solution here)
+insert into cars values (1, 'nissan', 1.12), (10, 'kia', null);
+-- END
---
+++ postgresql-ddl/05_insert/solution_teacher.sql
@@ -0,0 +1,4 @@
+﻿-- BEGIN
+INSERT INTO cars VALUES (1, 'nissan', 1.12);
+INSERT INTO cars (id, name) VALUES (10, 'kia');
+-- END
