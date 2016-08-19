097fdd47f746e38b38f82bfa4cec9c5182c419b1
Dmitry Fil <monkdaf@gmail.com>
2016-08-19 13:16:14 +0300

    Урок 3 hexlet_fs

---
+++ js_errors/03_hexlet_fs/HexletFS.js
@@ -0,0 +1,35 @@
+﻿import Tree from 'hexlet-trees';
+
+// BEGIN (write your solution here)
+const normalizePath = strPath => strPath.split('/').filter((elem) => elem !== '');
+// END
+
+export default class {
+  constructor() {
+    this.tree = new Tree('/');
+  }
+
+  // BEGIN (write your solution here)
+  isDirectory(path) {
+    const child =this.tree.getDeepChild(normalizePath(path));
+    return child === undefined ? false : child.getMeta().type !== 'dir' ? false : true;
+  }
+
+  isFile(path) {
+    const child =this.tree.getDeepChild(normalizePath(path));
+    return child === undefined ? false : child.getMeta().type !== 'file' ? false : true;
+  }
+
+  mkdirSync(path) {
+    let arrPath = normalizePath(path);
+    const newDirectory = arrPath.pop();
+    this.tree.getDeepChild(arrPath).addChild(newDirectory, {type: 'dir'});
+  }
+
+  touchSync(path) {
+    let arrPath = normalizePath(path);
+    const newFile = arrPath.pop();
+    this.tree.getDeepChild(arrPath).addChild(newFile, {type: 'file'});
+  }
+  // END
+}
---
+++ js_errors/03_hexlet_fs/HexletFS_teacher.js
@@ -0,0 +1,40 @@
+﻿import Tree from 'hexlet-trees';
+
+// BEGIN (write your solution here)
+const getPathParts = (path) =>
+  path.split('/').filter(part => part !== '');
+// END
+
+export default class {
+  constructor() {
+    this.tree = new Tree('/');
+  }
+
+  // BEGIN (write your solution here)
+  touchSync(path) {
+    const parts = getPathParts(path);
+    const name = parts[parts.length - 1];
+    const parent = this.tree.getDeepChild(parts.slice(0, -1));
+    return parent.addChild(name, { type: 'file' });
+  }
+
+  isFile(path) {
+    const parts = getPathParts(path);
+    const current = this.tree.getDeepChild(parts);
+    return current && current.getMeta().type === 'file';
+  }
+
+  mkdirSync(path) {
+    const parts = getPathParts(path);
+    const name = parts[parts.length - 1];
+    const parent = this.tree.getDeepChild(parts.slice(0, -1));
+    return parent.addChild(name, { type: 'dir' });
+  }
+
+  isDirectory(path) {
+    const parts = getPathParts(path);
+    const current = this.tree.getDeepChild(parts);
+    return current && current.getMeta().type === 'dir';
+  }  
+  // END
+}
