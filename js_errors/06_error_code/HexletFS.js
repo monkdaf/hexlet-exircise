87af850803e12115891e3bd3a908bb2ff0469111
Dmitry Fil <monkdaf@gmail.com>
2016-09-01 11:17:01 +0300

    Refactoring

--- js_errors/06_error_code/HexletFS.js
+++ js_errors/06_error_code/HexletFS.js
@@ -20,7 +20,47 @@ export default class {
   }
 
   // BEGIN (write your solution here)
+  unlinkSync(path) {
+    const parts = getPathParts(path);
+    const name = parts[parts.length - 1];
+    const current = this.tree.getDeepChild(parts);
+    if (!current) {
+      return [null, errors.code.ENOENT];
+    }
+    if (!current.getMeta().getStats().isFile()) {
+      return [null, errors.code.EPERM];
+    }
+    return [current.parent.removeChild(name), null];
+  }
   
+  writeFileSync(path, body) {
+    const parts = getPathParts(path);
+    const name = parts[parts.length - 1];
+    const current = this.tree.getDeepChild(parts);
+    const parent = this.tree.getDeepChild(parts.slice(0, -1));
+    if (!parent) {
+      return [null, errors.code.ENOENT];
+    }
+    if (current && current.getMeta().isDirectory()) {
+      return [null, errors.code.EISDIR];
+    }
+    if (current && current.getMeta().isFile()) {
+      this.unlinkSync(path);
+    }
+    return [parent.addChild(name, new File(name, body)), null];
+  }
+  
+  readFileSync(path) {
+    const parts = getPathParts(path);
+    const current = this.tree.getDeepChild(parts);
+    if (!current) {
+      return [null, errors.code.ENOENT];
+    }
+    if (!current.getMeta().getStats().isFile()) {
+      return [null, errors.code.EISDIR];
+    }
+    return [current.getMeta().getBody(), null];
+  }
   // END
 
   mkdirpSync(path) {
---
+++ js_errors/06_error_code/HexletFS_teacher.js
@@ -0,0 +1,39 @@
+﻿// BEGIN
+  unlinkSync(path) {
+    const parts = getPathParts(path);
+    const name = parts[parts.length - 1];
+    const current = this.tree.getDeepChild(parts);
+    if (!current) {
+      return [null, errors.code.ENOENT];
+    } else if (current.getMeta().isDirectory()) {
+      return [null, errors.code.EPERM];
+    }
+    return [current.parent.removeChild(name), null];
+  }
+
+  writeFileSync(path, body) {
+    const parts = getPathParts(path);
+    const name = parts[parts.length - 1];
+    const parent = this.tree.getDeepChild(parts.slice(0, -1));
+    if (!parent) {
+      return [null, errors.code.ENOENT];
+    }
+    const current = parent.getChild(name);
+    if (current && current.getMeta().isDirectory()) {
+      return [null, errors.code.EISDIR];
+    }
+    return [parent.addChild(name, new File(name, body)), null];
+  }
+
+  readFileSync(path) {
+    const current = this.tree.getDeepChild(getPathParts(path));
+    if (!current) {
+      return [null, errors.code.ENOENT];
+    }
+    if (current.getMeta().isDirectory()) {
+      return [null, errors.code.EISDIR];
+    }
+    return [current.getMeta().getBody(), null];
+  }
+
+  // END
