a232f2d8bbc061462b114e95c524b9e3cdb1aa60
Dmitry Fil <monkdaf@gmail.com>
2016-09-01 11:15:16 +0300

    Добавление заданий для chelllendges

---
+++ challenges/js_prototype_extract_nested_values/test.js
@@ -0,0 +1,23 @@
+﻿import assert from 'assert';
+import './solution';
+
+const obj = {
+  person: {
+    name: 'joe',
+    history: {
+      hometown: 'bratislava',
+      bio: {
+        funFact: 'I like fishing.',
+      },
+    },
+  },
+};
+
+describe('Hash', () => {
+  it('should work', () => {
+    assert.deepEqual(obj.hash('person.history.bio'), { funFact: 'I like fishing.' });
+    assert.deepEqual(obj.hash('person.name.key'), undefined);
+    assert.deepEqual(obj.hash('person.history.homeStreet'), undefined);
+    assert.deepEqual(obj.hash('person.animal.pet.needNoseAntEater'), undefined);
+  });
+});
---
+++ challenges/js_prototype_wrapped_function/test.js
@@ -0,0 +1,21 @@
+﻿import assert from 'assert';
+import './solution';
+
+function speak(name) {
+  return `Hello ${name}`;
+}
+
+describe('Wrap', () => {
+  it('should work', () => {
+    const newSpeak = speak.wrap((original, yourName, myName) => {
+      const greeting = original(yourName);
+      return `${greeting}, my name is ${myName}`;
+    });
+
+    const greeting1 = newSpeak('Mary', 'Kate');
+    assert.equal(greeting1, 'Hello Mary, my name is Kate');
+
+    const greeting2 = newSpeak('Kate', 'Mary');
+    assert.equal(greeting2, 'Hello Kate, my name is Mary');
+  });
+});
