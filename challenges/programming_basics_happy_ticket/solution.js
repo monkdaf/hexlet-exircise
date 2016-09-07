a8533fa2e28fcd3c62add4906718aaa2db944444
Dmitry Fil <monkdaf@gmail.com>
2016-09-07 10:17:04 +0300

    Add chalenge programming_basics_happy_ticket

---
+++ challenges/programming_basics_happy_ticket/solution.js
@@ -0,0 +1,5 @@
+﻿import * as strings from './strings';
+
+// BEGIN (write your solution here) (write your solution here)
+
+// END
---
+++ challenges/programming_basics_happy_ticket/test.js
@@ -0,0 +1,16 @@
+﻿/* global define, it, describe */
+
+import assert from 'assert';
+import isHappyTicket from './solution';
+
+describe('ackermann', () => {
+  it('is happy ticket', () => {
+    assert.equal(true, isHappyTicket(123321));
+    assert.equal(true, isHappyTicket(341800));
+    assert.equal(true, isHappyTicket(812146));
+  });
+  it('is not happy ticket', () => {
+    assert.equal(false, isHappyTicket(123567));
+    assert.equal(false, isHappyTicket(213612));
+  });
+});
